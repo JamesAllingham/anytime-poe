9c2b8c415d1c8bb7020d2b0f6fb28af03c22c90c
JamesAllingham <james.allingham@gmail.com>
2022-05-22 13:50:24 +0100

    Formatting

--- src/models/common.py
+++ src/models/common.py
@@ -16,25 +16,25 @@ def raise_if_not_in_list(val, valid_options, varname):
 
 
 def get_locs_scales_probs(
-        obj,
-        x: Array,
-        train: bool = False,
-    ):
-        ens_preds = jnp.stack([net(x, train=train) for net in obj.nets], axis=0)  # (M, O * 2)
-        M, _ = ens_preds.shape
-        ens_preds = ens_preds.reshape(M, -1, 2)  # (M, O, 2)
+    obj,
+    x: Array,
+    train: bool = False,
+):
+    ens_preds = jnp.stack([net(x, train=train) for net in obj.nets], axis=0)  # (M, O * 2)
+    M, _ = ens_preds.shape
+    ens_preds = ens_preds.reshape(M, -1, 2)  # (M, O, 2)
 
-        locs = ens_preds[:, :, 0]  # (M, O)
+    locs = ens_preds[:, :, 0]  # (M, O)
 
-        if obj.noise == 'hetero':
-            log_scales = ens_preds[:, :, 1]  # (M, O)
-            scales = jnp.exp(log_scales)
-        elif obj.noise == 'homo-per-ens':
-            scales = jnp.exp(obj.logscale)  # (M, O)
-        else:
-            scales = jnp.exp(obj.logscale)[jnp.newaxis, :]  # (M, O)
+    if obj.noise == 'hetero':
+        log_scales = ens_preds[:, :, 1]  # (M, O)
+        scales = jnp.exp(log_scales)
+    elif obj.noise == 'homo-per-ens':
+        scales = jnp.exp(obj.logscale)  # (M, O)
+    else:
+        scales = jnp.exp(obj.logscale)[jnp.newaxis, :]  # (M, O)
 
-        weights = obj.weights if obj.learn_weights else jax.lax.stop_gradient(obj.weights)
-        probs = nn.softmax(weights)[:, jnp.newaxis]
+    weights = obj.weights if obj.learn_weights else jax.lax.stop_gradient(obj.weights)
+    probs = nn.softmax(weights)[:, jnp.newaxis]
 
-        return locs, scales, probs
+    return locs, scales, probs
