e7287ae4fbcee827a8a59c0c8297f8efe6705a98
JamesAllingham <james.allingham@gmail.com>
2022-05-22 13:11:46 +0100

    Add regression ensemble

---
+++ src/models/reg_ens.py
@@ -0,0 +1,78 @@
+from typing import Callable
+
+import jax
+import flax.linen as nn
+from flax.linen import initializers
+from chex import Array
+import distrax
+
+from src.models.common import raise_if_not_in_list, NOISE_TYPES, get_locs_scales_probs
+
+
+class Reg_Ens(nn.Module):
+    """A standard regression ensemble."""
+    size: int
+    make_net: Callable[[], nn.Module]
+    weights_init: Callable = initializers.ones
+    logscale_init: Callable = initializers.zeros
+    noise: str = 'homo'
+    learn_weights: bool = False
+
+    def setup(self):
+        raise_if_not_in_list(self.noise, NOISE_TYPES, 'self.noise')
+
+        self.nets = [self.make_net() for _ in range(self.size)]
+        self.weights = self.param(
+            'weights',
+            self.weights_init,
+            (self.size,)
+        )
+        if self.noise != 'hetero':
+            self.logscale = self.param(
+                'logscale',
+                self.logscale_init,
+                (self.nets[0].out_size // 2,) if self.noise == 'homo' else (self.size, self.net.out_size // 2,)
+            )
+
+    def __call__(
+        self,
+        x: Array,
+        train: bool = False,
+    ) -> Array:
+        locs, scales, probs = get_locs_scales_probs(self, x, train)
+
+        return locs, scales, probs
+
+
+def make_Reg_Ens_loss(
+    model: Reg_Ens,
+    x_batch: Array,
+    y_batch: Array,
+    train: bool = True,
+) -> Callable:
+    """Creates a loss function for training a std Ens."""
+    def batch_loss(params, state):
+        # define loss func for 1 example
+        def loss_fn(params, x, y):
+            (locs, scales, probs), new_state = model.apply(
+                {"params": params, **state}, x, train=train,
+                mutable=list(state.keys()) if train else {},
+            )
+
+            def nll(y, loc, scale):
+                return  -1 * distrax.Normal(loc, scale).log_prob(y)
+
+            nlls = jax.vmap(nll, in_axes=(None, 0, 0))(y, locs, scales)
+            loss = (nlls * probs).sum(axis=0)
+
+            sq_err = ((locs * probs).sum(axis=0) - y)**2
+
+            return loss, sq_err, new_state
+
+        # broadcast over batch and take mean
+        loss_for_batch, errors_for_batch, new_state = jax.vmap(
+            loss_fn, out_axes=(0, 0, None), in_axes=(None, 0, 0), axis_name="batch"
+        )(params, x_batch, y_batch)
+        return loss_for_batch.mean(axis=0), (errors_for_batch.mean(axis=0), new_state)
+
+    return batch_loss
